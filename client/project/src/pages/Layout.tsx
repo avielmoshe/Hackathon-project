d44101d2532d2f4ef6f13052e340f51a135098fe
Ben Kilinski <ben.kilinski@gmail.com>
2024-12-18 23:12:41 +0200

    ff

--- client/project/src/pages/Layout.tsx
+++ client/project/src/pages/Layout.tsx
@@ -1,16 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "@/components/Footer";
-import EditProfile from "@/components/EditProfile";
+
 function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
-    <Header />
-    <main className="flex-grow bg-background text-foreground">
-      <Outlet />
-    </main>
-    <Footer />
-  </div>
+      {/* Header (Navbar) sempre no topo */}
+      <Header />
+
+      {/* Main Content */}
+      <main className="flex-grow bg-background text-foreground">
+        <Outlet />
+      </main>
+
+      {/* Footer sempre no final */}
+      <Footer />
+    </div>
   );
 }
 
