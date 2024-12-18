f762bcaf13e10e32ab66234bb4df0a1dc14dcb51
eden shabi <eden9876102@gmail.com>
2024-12-18 15:01:56 +0200

    up

--- client/project/tailwind.config.js
+++ client/project/tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class', // Ativa o modo escuro com a classe 'dark'
+  darkMode: "class", // Ativa o modo escuro com a classe 'dark'
   theme: {
     extend: {
       colors: {
         sun: {
-          background: '#A294F9',
-          primary: '#E5D9F2',
-          secondary: '#CDC1FF',
-          accent: '#F5EFFF',
+          background: "#A294F9",
+          primary: "#E5D9F2",
+          secondary: "#CDC1FF",
+          accent: "#F5EFFF",
         },
         dark: {
-          background: '#213555',
-          primary: '#3E5879',
-          secondary: '#D8C4B6',
-          accent: '#F5EFE7',
+          background: "#213555",
+          primary: "#3E5879",
+          secondary: "#D8C4B6",
+          accent: "#F5EFE7",
         },
+      },
+      backgroundImage: {
+        banner: "url('/src/assets/images/banner-background.jpg')",
       },
     },
   },
