710ea9ecc50f430fc4a651516c6902b9585911f3
eden shabi <eden9876102@gmail.com>
2024-12-18 22:22:28 +0200

    up

--- client/project/src/pages/Profile.tsx
+++ client/project/src/pages/Profile.tsx
@@ -1,19 +1,20 @@
-import { RootState } from "@/store";
 import { getProviderByUserId } from "@/utils/api.service";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 
+import defaultBanner from "../assets/images/banner-background.jpg";
+
 function Profile() {
+  const [profileData, setProfileData] = useState("");
   const params = useParams();
   const navigate = useNavigate();
-  const user = useSelector((state: RootState) => state.user.user);
 
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     (async () => {
       const providerData = await getProviderByUserId(params.id);
+      setProfileData(providerData);
       console.log(providerData);
 
       if (providerData.dontHaveData) {
@@ -23,7 +24,6 @@ function Profile() {
       }
     })();
   }, [params.id, navigate]);
-
   if (!isLoaded) {
     return (
       <div className="h-screen w-screen flex items-center justify-center">
@@ -31,32 +31,56 @@ function Profile() {
       </div>
     );
   }
+  console.log("abc", profileData);
 
   return (
     <div className="h-screen w-screen md:flex flex-col items-center">
-      <div className="w-full h-72 bg-banner bg-cover bg-center mb-10"></div>
+      <div
+        className="w-full h-72 bg-cover bg-center mb-10"
+        style={{
+          backgroundImage: `url(${profileData.bannerImg || defaultBanner})`,
+        }}
+      ></div>
       <div className="md:flex flex-col items-start w-screen h-screen">
         <div className="md:flex w-screen relative">
-          <div className="rounded-full bg-black w-36 h-36 ml-32"></div>
+          <div>
+            <div className="rounded-full bg-black w-36 h-36 ml-32"></div>
+            <p className="ml-40 mt-4">rating: 0</p>
+          </div>
           <div className="md:flex flex-col ml-12">
             <h1 className="md:flex flex-col font-bold text-3xl">
-              {user.firstName &&
-                user.firstName.charAt(0).toUpperCase() +
-                  user.firstName.slice(1).toLowerCase()}{" "}
-              {user.lastName &&
-                user.lastName.charAt(0).toUpperCase() +
-                  user.lastName.slice(1).toLowerCase()}
+              {profileData.userID?.username &&
+                profileData.userID?.username.charAt(0).toUpperCase() +
+                  profileData.userID?.username.slice(1).toLowerCase()}{" "}
             </h1>
-            <p>{user.username}</p>
-            <p>{user.email}</p>
-            <p>{user.phone}</p>
-            <p>bio: {user.bio}</p>
+            <p>Location: {profileData.location}</p>
+            <p>Email: {profileData.userID?.email}</p>
+            <p>Mobile number: {profileData.userID?.phone}</p>
+            {profileData.webLink && (
+              <p>
+                Website:{" "}
+                <span
+                  className="text-blue-700"
+                  onClick={() => navigate(profileData.webLink)}
+                >
+                  {profileData.webLink}
+                </span>
+              </p>
+            )}
+            <p>
+              bio:
+              <div className="bg-gray-50 text-black h-80 w-96 rounded-xl ml-10 mb-10">
+                {profileData.bio && <p className="p-2"> {profileData.bio}</p>}
+              </div>
+            </p>
           </div>
-          <button className="bg-gray-200 absolute top-0 right-10 cursor-pointer">
+          <button
+            className="bg-gray-200 absolute top-0 right-10 cursor-pointer"
+            onClick={() => navigate("/EditProfile")}
+          >
             edit
           </button>
         </div>
-        <p className="ml-40 mt-4">rating: 0</p>
       </div>
     </div>
   );
